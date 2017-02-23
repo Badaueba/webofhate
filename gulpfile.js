08cbde5e4e2dfa657e4823eede6b80272a026e2c
badaueba <alan.demit@hotmail.com>
2017-02-23 17:12:10 -0300

    watchify

--- gulpfile.js
+++ gulpfile.js
@@ -5,16 +5,68 @@ var rename = require('gulp-rename');
 var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
 var browserify = require('browserify');
+var watchify = require('watchify');
 
 var path = {
     src : 'public/game',
     dest : 'public/'
 };
-gulp.task('bundle', function (){
-    browserify(path.src).bundle()
+
+function browserifyTask() {
+    return browserify({
+        cache: {},
+        packageCache: {},
+        debug: true,
+        entries: path.src
+    })
+    .bundle()
+    .pipe({
+        cache : {}
+    })
+    .on('error', function(e){
+      gutil.log(e);
+    })
+    .pipe(source('webofhate.js'))
+    .pipe(gulp.dest('./public'))
+}
+
+gulp.task('browserify', browserifyTask);
+
+function bundle (bundler){
+    return bundler
+    .bundle()
     .on('error', function (e) {
         gutil.log(e);
     })
-    .pipe(source('webofhate.js'))
-    .pipe(gulp.dest('./public/'));
+    // .pipe({
+    //     cache : {}
+    // })
+    .pipe(source('webofhate.js'))    
+    .pipe(gulp.dest('./public/'))
+
+}
+
+gulp.task('js', function () {
+    
+    // var watcher = watchify(browserifyTask(path.src, watchify.args));
+    var watcher = watchify(browserify({
+        cache: {},
+        packageCache: {},
+        debug: true,
+        entries: path.src
+    }));
+
+    bundle(watcher);
+
+    watcher.on('update', function () {
+        // browserifyTask();
+        bundle(watcher);
+        gutil.log();
+    });
+    
+    watcher.on('log', function() {
+        gutil.log();
+    });
 });
+
+gulp.task('default', ['js']);
